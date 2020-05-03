f41283ac10ecaa53112b3c7a2bfb5e63df11d15c
dawk42 <derek.vanvuuren@gmail.com>
2020-05-02 17:01:13 -0700

    latest pw gen

--- ch4/ch4ec.py
+++ ch4/ch4ec.py
@@ -1,17 +1,28 @@
+import random
+
 from random import randint
+
+from random import seed
+
+from datetime import datetime
+
 # lets us have random integers
 
-num = ['1','2','3','4','5','6','7','8','9','0']
+num = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 # defines the number list
-sym = ['!','@','#','$','%','^','&','*','(',')','`','~','-','_','=','+','[',']','{','}','\\','|',';',':',"'",'"',',','.','<','>','/','?']
+sym = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '`',
+       '~', '-', '_', '=', '+', '[', ']', '{', '}', '\\', '|',
+       ';', ':', "'", '"', ',', '.', '<', '>', '/', '?']
 # defines the symbol list
-low = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',]
+low = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+       'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 #defines the lowercase alphabet
-upp = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',]
+upp = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+       'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 #defines the upcase alphabet
 pw = ""
 alpha = low + upp
-charset = ""
+charset = []
 
 def choice():
     global dif
@@ -19,10 +30,10 @@ def choice():
     dif = int(input("Enter Desired Password Length 8 - 300 characters:\n"))
     while dif < 8 or dif > 300: # makes sure password length is correct
         choice()
-        if dif >= 8 and dif <= 300:
+        if  8 <= dif <= 300:
             continue
     options()
-    
+
 def options():
     global charset
     print("PASSWORD GENERATOR QUIZ!!!\n")
@@ -37,9 +48,7 @@ def upper():
     global upp
     option = str(input('Upper case y/n: '))
     if option == 'y':
-        temp = charset + upp
-        charset = temp
-        print ("Passed Upper")
+        charset += upp
         lower()
     else:
         lower()
@@ -49,8 +58,7 @@ def lower():
     global low
     option = str(input('Lower case y/n: '))
     if option == 'y':
-        temp = charset + low
-        charset = temp
+        charset += low
         numb()
     else:
         numb()
@@ -60,10 +68,9 @@ def numb():
     option = str(input('Numbers? y/n: '))
     if option == 'y':
         charset += num
-        print(charset)
-        sym()
+        symb()
     else:
-        sym()
+        symb()
 
 def symb():
     global charset
@@ -71,55 +78,60 @@ def symb():
     option = str(input('Symbols? y/n: '))
     if option == 'y':
         charset += sym
-        print(charset)
-        ecx1()
+        print ("Symbols Included")
+        exc1()
     else:
-        ecx1()
+        exc1()
 
 def exc1():
     global charset
     option = str(input('Leave out i, l, L, 1 and !? y/n: '))
     if option == 'y':
-        exclude = {'i','l','L','1','!'}
+        exclude = {'i', 'l', 'L', '1', '!'}
         charset = [char for char in charset if char not in exclude]
-        print(charset)
-        ecx2()
+        print("Removing Similar characters...")
+        exc2()
     else:
-        ecx2()
+        exc2()
 
 def exc2():
     global charset
     option = str(input('Leave out  {}[]()/\\!\'",;:>,.? y/n: '))
     if option == 'y':
-        exclude = {'{','}','[',']','(',')''/','\\','"',"'",'!',':',';','>','<',',','.','?'}
+        exclude = {'{', '}', '[', ']', '(', ')', '/', '\\', '"', "'",
+                   '!', ':', ';', '>', '<', ',', '.', '?'}
         charset = [char for char in charset if char not in exclude]
 
-        print(charset)
-        ecx2()
+        print("Removing some symbols...")
+        count()
+        #pwgen()
     else:
-        ecx2()
-
+        count()
+        #pwgen()
 #  generate multiple passwords to choose from
 def count():
-    option = str(input('How many passwords do you want to generate?: '))
-    print(pwgen()* option)
+    option = int(input('How many passwords do you want to generate?: '))
+    while option > 0:
+        pwgen()
+        option -= 1
 
 def fchar():
     global pw
     global dif
-    pw = alpha[(randint(1,len(alpha))-1)]
+    pw = alpha[(randint(1, len(alpha))-1)]
     dif -= 1
     upper()
 
 def pwgen():
+    random.seed(datetime.now())
     global dif
     global pw
-    #pwlen = randint(1,dif)
-    size = len(charset)
     while dif > 0:
-        pw += charset[(randint(1,len(charset)-1))]
+        pw += charset[(randint(1, len(charset)-1))]
         dif -= 1
-        print (pw)
+        #print (pw)
+        continue
+    print(pw)
 
 choice()
 #print(charset)
