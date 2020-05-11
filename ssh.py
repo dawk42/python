15af349d99a99c987e4a96ec4b2b012bb64497e6
dawk42 <derek.vanvuuren@gmail.com>
2020-05-10 23:08:23 -0700

    pxssh

--- ssh.py
+++ ssh.py
@@ -3,21 +3,36 @@ def connect():
     from pexpect import pxssh
     import getpass
     try:
-        ip = "192.168.142.241"
-        un = "student"
-        #password = getpass.getpass('password:')
-        pw = "Password01"
-        tx = pxssh.pxssh()
-        tx.login(ip, un, pw, sync_multiplier=5)
-        tx.sync_original_prompt()
+        ip = "192.168.142.232" #for testing
+        #ip = str(input("Enter the hostname/IP:\n"))
+        un = "student" #for testing
+        #un = str(input("Enter the username:\n"))
+        #password = getpass.getpass("Enter the password:\n")
+        pw = "Password01" #for testing
+        tx = pxssh.pxssh()  #simplify the code by placing the function into variable
+        tx.login(ip, un, pw, sync_multiplier=5)  #login using previous input
+        tx.sync_original_prompt()  #make sure we have the prompt identified
      #   tx.set_unique_prompt()
-        tx.sendline ('sudo su')
-        tx.expect("password for", timeout=10)
+        tx.sendline ('sudo useradd egoad')  #sends command
+        tx.expect ("password .*:", timeout=5)
         tx.sendline(pw)
-        tx.prompt()
-        tx.sendline('dnf install httpd -y')
+        tx.sendline ('sudo passwd egoad')
+        tx.expect('New password:')
+        tx.sendline ('RubberDuck!')
+        tx.expect('Retype .*:')
+        tx.sendline ('RubberDuck!')
+        tx.expect('passwd: .*')
         tx.prompt()
         print(tx.before)
+
+        
+        tx.sendline ('sudo dnf install httpd -y')
+        #tx.expect("password for", timeout=10)
+        #tx.sendline(pw)
+        #tx.prompt()
+        #tx.sendline('')
+        #tx.prompt()
+        #print(tx.before)
         tx.logout()
         print('Done')
         #print(username)
