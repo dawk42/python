9d72e0fff9741f5552ca7a26271bfcc2d12cb4f0
dawk42 <derek.vanvuuren@gmail.com>
2020-05-13 09:26:08 -0700

    cleanup

--- db.py
+++ db.py
@@ -14,25 +14,14 @@ def db_setting():
 def db(un, ip, pw):
 
     try:
-        #ip = "192.168.142.232" #for testing
-        #ip = str(input("Enter the hostname/IP:\n"))
-        #un = "student" #for testing
-        #un = str(input("Enter the username:\n"))
-        #password = getpass.getpass("Enter the password:\n")
-        #pw = "Password01" #for testing
         tx = pxssh.pxssh()  #simplify the code by placing the function into variable
         tx.login(ip, un, pw, sync_multiplier=5)  #login using previous input
         tx.sync_original_prompt()  #make sure we have the prompt identified
-     #   tx.set_unique_prompt()
-
-     
-        
         tx.sendline ('sudo dnf -y install MariaDB-server')
         tx.expect ("password .*:", timeout=5)
         tx.sendline(pw)
         tx.expect(".*Complete!", timeout=240)
         print("Installed MariaDB Server")
-        #tx.sendline(pw)
         tx.prompt()
         tx.sendline('sudo systemctl start httpd')
         tx.prompt()
@@ -44,7 +33,6 @@ def db(un, ip, pw):
         tx.expect('.*active.*')
         print('Service mariadb start verified')
         tx.close(force=True)
-        #tx.prompt()
         tx.prompt(tx, timeout=-1)
         tx.logout()
         tx.close()
--- web.py
+++ web.py
@@ -14,18 +14,10 @@ def web_setting():
 def web(un, ip, pw):
 
     try:
-        #ip = "192.168.142.232" #for testing
-        #ip = str(input("Enter the hostname/IP:\n"))
-        #un = "student" #for testing
-        #un = str(input("Enter the username:\n"))
-        #password = getpass.getpass("Enter the password:\n")
-        #pw = "Password01" #for testing
         tx = pxssh.pxssh()  #simplify the code by placing the function into variable
         tx.login(ip, un, pw, sync_multiplier=5)  #login using previous input
         tx.sync_original_prompt()  #make sure we have the prompt identified
-     #   tx.set_unique_prompt()
         tx.sendline ('sudo useradd egoad')  #sends command
-
         tx.expect ("password .*:", timeout=5)
         tx.sendline(pw)
         tx.sendline ('sudo passwd egoad')
@@ -36,12 +28,9 @@ def web(un, ip, pw):
         tx.expect('passwd: .*')
         tx.prompt()
         print("Created User egoad with a secret password!")
-
-        
         tx.sendline ('sudo dnf install httpd -y')
         tx.expect(".*Complete!", timeout=120)
         print("Installed Web Server")
-        #tx.sendline(pw)
         tx.prompt()
         tx.sendline('sudo systemctl start httpd')
         tx.prompt()
@@ -53,7 +42,6 @@ def web(un, ip, pw):
         tx.expect('.*active.*')
         print('Service httpd start verified')
         tx.close(force=True)
-        #tx.prompt()
         tx.prompt(tx, timeout=-1)
         tx.logout()
         tx.close()
